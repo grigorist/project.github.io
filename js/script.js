06a758f2465aafb6628d7ecb9c1cf745be2edec5
Grigory <grishaosher@gmail.com>
2020-06-06 04:10:43 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/* function Menu () {
+    const menuBtn = document.querySelector(".m-menu");
+    const menu = document.querySelector(".menu-sidebar");
+    
+    menuBtn.addEventListener("click", function () {
+        menu.classList.toggle("active");
+    });  
+} */
+
+const menuBtn = document.querySelector(".m-menu");
+const menu = document.querySelector(".menu-sidebar");
+
+menuBtn.addEventListener("click", function () {
+    menu.classList.toggle("active");
+}); 
